e8a2f904357bee72f2da75eb209f760cb644a6b4
manauref <mfrancisquez@gmail.com>
2026-06-08 21:34:02 -0700

    Begin GK RZ plot command

--- src/postgkyl/commands/__init__.py
+++ src/postgkyl/commands/__init__.py
@@ -33,6 +33,7 @@ from postgkyl.commands.gk_energy_balance import gk_energy_balance
 from postgkyl.commands.gk_distf import load_gk_distf
 from postgkyl.commands.gk_distf import gk_distf
 from postgkyl.commands.gk_particle_balance import gk_particle_balance
+from postgkyl.commands.gk_rz import gk_rz
 from postgkyl.commands.perprotate import perprotate
 from postgkyl.commands.plot import plot
 from postgkyl.commands.pr import pr
